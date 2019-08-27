f1829c47f4b5417283e4775601d3efc9d4b938b6
Kyle Luke <th.oldschool@gmail.com>
2019-08-27 23:47:38 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,6 @@
 class Shoe
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+  attr_accessor :brand, :color, :size, :material, :condition
+  attr_reader
   
   def initialize(brand)
     @brand
