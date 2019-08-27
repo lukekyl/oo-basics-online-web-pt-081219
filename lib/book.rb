ee572493bcc5e5a65094b6dabbc9af0971a964d7
Kyle Luke <th.oldschool@gmail.com>
2019-08-27 23:28:22 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,36 +2,11 @@
 
 class Book 
   
+  attr_accessor :title, :page_count, :genre
+  
   def initialize(title)
     @title = title
   end
   
-  def title
-    @title 
-  end 
-  
-  def author=(name)
-    @author = name 
-  end
-  
-  def author 
-    @author 
-  end
-  
-  def page_count=(pages)
-    @pages = pages
-  end
-  
-  def page_count
-    @pages
-  end
-  
-  def genre=(genre)
-    @genre = genre 
-  end
-  
-  def genre
-    @genre
-  end
   
 end
